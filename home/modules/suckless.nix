4ad1587f8a40caefd69b5d0fc5977fb8b9953dc8
Joaquim Diaz <diazjoaquim@gmail.com>
2025-12-07 18:50:05 +0100

    [UPD] removed st

--- home/modules/suckless.nix
+++ home/modules/suckless.nix
@@ -2,11 +2,6 @@
 { pkgs, dotfiles, ... }:
 {
   home.packages = with pkgs; [
-    # st
-    (pkgs.st.overrideAttrs (_: {
-      src = "${dotfiles}/st";
-    }))
-
     # dmenu
     (pkgs.dmenu.overrideAttrs (_: {
       src = "${dotfiles}/dmenu";
